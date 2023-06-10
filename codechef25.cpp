eaeaf14289fd0aa2d2da463c94a2bb1ab62b6fa1
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2023-06-10 15:26:16 +0530

    Add files via upload

---
+++ codechef25.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		long long int a,b;
+		cin>>a>>b;
+		cout<<max(a,b)<<" ";
+		cout<<a+b<<endl;
+		
+		}
+		
+		
+		
+
+	
+	
+	
+	
+	return 0;
+}
