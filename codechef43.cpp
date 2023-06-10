ba3f0017b892affa9353b22fa98f0a34e41afbc8
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2023-06-10 15:32:04 +0530

    Add files via upload

---
+++ codechef43.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int num;
+		cin>>num;
+		if(num%2==0){
+			cout<<num<<endl;
+		}
+		else{
+			cout<<num-1<<endl;
+		}
+		
+		
+	
+	}
+	
+	
+	
+	
+	return 0;
+}
