51309d9576e601a1403a526fc3c38c09faa6a338
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2023-06-10 15:23:02 +0530

    Add files via upload

---
+++ codechef19.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+	int t;
+	cin>>t;
+	int num;
+	while(t--){
+		cin>>num;
+		cout<<int(sqrt(num))<<endl;
+	}
+	
+	
+	return 0;
+}
