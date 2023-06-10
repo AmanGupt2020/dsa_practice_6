195b56174293a488b50c668e5e82c3e6a9f57449
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2023-06-10 15:31:53 +0530

    Add files via upload

---
+++ codechef42.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int num;  
+		cin>>num;  
+		int c;
+		int num1;
+		 num1=num/2;  
+		c= num1*(num1-1);
+		c=c/2; 
+		cout<<c<<endl;
+		
+	}
+	
+	
+	return 0;
+}
