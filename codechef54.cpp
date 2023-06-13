4284b5400937435001dc9ea2f058a4a792bd3534
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2023-06-13 21:55:10 +0530

    Add files via upload

---
+++ codechef54.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int  a,c;
+		float b;
+		cin>>a>>b>>c;
+		if(a>50 && b<0.7 && c>5600)
+			cout<<"10"<<endl;
+			else if(a>50 && b<0.7 && c<=5600)
+			cout<<"9"<<endl;
+			else if(a<=50 && b<0.7 && c>5600)
+			cout<<"8"<<endl;
+		else if(a>50 && b>=0.7 && c>5600)
+			cout<<"7"<<endl;
+			else if(a>50 && b>=0.7 && c<=5600)
+			cout<<"6"<<endl;
+			else if(a<=50 && b<0.7 && c<=5600)
+			cout<<"6"<<endl;
+			else if(a<=50 && b>=0.7 && c>5600)
+			cout<<"6"<<endl;
+			else if(a<=50 && b>=0.7 && c<=5600)
+			cout<<"5"<<endl;
+	}
+	
+	
+	
+	return 0;
+}
