9240bebe89727eb48cde5fbb456d3b7c95dda823
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2023-06-18 21:56:15 +0530

    Add files via upload

---
+++ codechef91.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		int a[n];
+		int c=0;
+		for(int i=0;i<n;i++){
+			cin>>a[i];
+		}
+		for(int i=0;i<n;i++){
+			if(a[i]==1){
+				c++;
+			}
+		}
+		if(c%2==0){
+			cout<<n-c<<endl;
+		}
+		else{
+			cout<<c<<endl;
+		}
+		
+	}
+	return 0;
+}
+
