73cb5dde04017fc6854b16e73f640ddd405cce5b
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2023-06-24 23:44:44 +0530

    Add files via upload

---
+++ equillibrium_num.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int arr[100];
+	int n;
+	cin>>n;
+	int m;
+	
+	for(int i=0;i<n;i++){
+		cout<<"length :";
+		cin>>m;
+		for(int j=0;j<m;j++){
+			cin>>arr[j];
+		}
+		int sum1=0;
+	    int sum2=0;
+		
+		int low=0;
+		int high= m-1; 
+		int mid=(low+high)/2;
+		for(int k=0;k<mid;k++){
+			sum1=sum1+arr[k];  
+			
+		}
+		for(int l=mid+1;l<=high;l++){  
+			sum2= sum2+arr[l];
+		}
+	if(sum1==sum2){
+		cout<<"equillibrium possible at "<<arr[mid]<<endl;
+	}
+	else{
+		cout<<"no eqillibrium"<<endl;
+	}			
+		}	
+ return 0;
+}
