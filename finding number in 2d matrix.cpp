e994cb24f41fa8fc39c5114d1d3c527dace64a69
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2023-06-28 11:42:50 +0530

    Add files via upload

---
+++ finding number in 2d matrix.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int arr[4][4]={ 
+	                {1,2,3,4},
+			        {5,6,7,8},
+			        {9,10,11,12},
+			        {13,14,15,16}
+	    }
+	    int n= 4;
+	    int target=11;
+	    int row= 0;
+	    int col= n-1;
+	 bool found=false;
+	 while(col>=0 && row<n-1){
+	 	if(target==arr[row][col]){
+	 		found=true;
+		 }
+		 else if(target<arr[row][col]){
+		 	col--;
+		 }
+		 else{
+		 	row++;
+		 }
+	 }
+	 cout<<(found ? "found":"not found")<<endl;
+	
+	return 0;
+}
