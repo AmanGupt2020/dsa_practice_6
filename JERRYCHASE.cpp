d179eaaac6f786772c0b9e381ccfad3feb1e450e
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2023-08-03 23:25:25 +0530

    Add files via upload

---
+++ JERRYCHASE.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	while(n--){
+	    int x,y;
+	    cin>>x>>y;
+	    (y>x)?cout<<"YES"<<endl:cout<<"NO"<<endl;
+	}
+	return 0;
+}
