53ffead84fd7411aa7199fee6f2ecf54f64b9bf3
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2023-08-03 23:25:11 +0530

    Add files via upload

---
+++ BTRYHLTH.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    (x>=80)?cout<<"YES"<<endl:cout<<"NO"<<endl;
+	}
+	return 0;
+}
