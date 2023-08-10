96eb6e032ded5078e860af87a120a45b849d6a0c
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2023-08-10 23:30:12 +0530

    Add files via upload

--- PRACTICEPERF.cpp
+++ PRACTICEPERF.cpp
@@ -2,14 +2,15 @@
 using namespace std;
 
 int main() {
-	int arr[4];
-	int count=0;
+    int s=0;
+    
 	for(int i=0;i<4;i++){
-	    cin>>arr[i];
-	    if(arr[i]>=10){
-	        count++;
+	    int a;
+	    cin>>a;
+	    if(a>=10){
+	        s++;
 	    }
 	}
-	cout<<count<<endl;
+	cout<<s<<endl;
 	return 0;
 }
