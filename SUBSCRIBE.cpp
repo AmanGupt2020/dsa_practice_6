ea0f5288961fc50432294a9d79ab78b50b0b2deb
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2023-08-27 16:45:31 +0530

    Add files via upload

---
+++ SUBSCRIBE.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a;
+	cin>>a;
+	while(a--){
+	    int n;
+	    cin>>n;
+	    if(n>30){cout<<"YES"<<endl;}else{cout<<"NO"<<endl;}
+	}
+	return 0;
+}
