6ece0f8a58e44eedd8e3b13488032c75e8499e27
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2023-08-31 19:55:27 +0530

    Add files via upload

---
+++ ONEMORE.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	while(n--){
+	    int a;
+	    cin>>a;
+	    if(a<25){cout<<"NO"<<endl;}
+	    else{
+	        cout<<"YES"<<endl;
+	    }
+	}
+	return 0;
+}
