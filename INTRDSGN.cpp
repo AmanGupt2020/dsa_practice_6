7616aea52e2b5512285c6d04d0cede3be4eda2f5
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2023-10-01 22:49:52 +0530

    Add files via upload

---
+++ INTRDSGN.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	while(n--){
+	    int a,b,c,d;
+	    cin>>a>>b>>c>>d;
+	    cout<<min(a+b,c+d)<<endl;
+	}
+	return 0;
+}
