9f4515aa9461e9b85070d42e442f6bf774490a76
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2023-10-09 13:13:38 +0530

    commit

---
+++ CWIREFRAME.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	while(n--){
+	    int a,b,c;
+	    cin>>a>>b>>c;
+	    int m=a*2+b*2;
+	    cout<<m*c<<endl;
+	}
+	return 0;
+}
