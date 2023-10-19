569c9f98a53ec63686759e92386a7fa81fdebf78
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2023-10-19 23:53:14 +0530

    commit

---
+++ 123.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<" _        _"<<endl;
+	cout<<"|   6 5 4  |"<<endl;
+	cout<<"|   4 6 9  |"<<endl;
+	cout<<"|_  8 9 4 _|"<<endl;
+	
+	return 0;
+}
