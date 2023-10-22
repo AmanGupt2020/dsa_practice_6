f87768b8585f6273989deb3675dea70e8cc66da6
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2023-10-22 23:56:30 +0530

    commit

---
+++ 12345.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a[5];
+	int n=5;
+	for(int i=0; i<n ; i++)
+	cin<<A[i];
+	for(int i = 0 ; i<n-1 ; i++){
+	
+		min= i;
+		for(int j=i+1 ; j<n ; J++){
+			if(A[j]<A[min]){
+				min = j;
+			}
+		}
+		int temp = A[i];
+		A[i]= A[min];
+		A[min]= temp
+	}
+	for(int i=0; i<n ; i++)
+	cout>>A[i];
+}
