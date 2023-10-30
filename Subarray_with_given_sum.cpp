348855ffc75427fb0dae632bd97886c352774947
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2023-10-30 22:54:50 +0530

    commit

---
+++ Subarray_with_given_sum.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+    public:
+    //Function to find a continuous sub-array which adds up to a given number.
+    vector<int> subarraySum(vector<int>arr, int n, long long s)
+    {
+         int l =0;
+        int sum = 0;
+
+        for(int i=0;i < n;i++){
+            sum += arr[i];
+            if(sum == s){
+                return {l+1, i+1};
+            } else if(sum > s) {
+                while(sum > s){
+                    sum -= arr[l];
+                    l++;
+                }
+                if(sum == s && l <= i){
+                    return {l+1, i+1};
+                }
+            }
+        }
+        return {-1};
+    }
+};
