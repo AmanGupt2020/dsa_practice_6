2ee8f826ab675bf4285c186dd10103457cfe6847
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2023-10-30 22:50:00 +0530

    commit

---
+++ Minimum_number_of_jumps.cpp
@@ -0,0 +1,29 @@
+// Function to return minimum number of jumps to end of array
+
+class Solution{
+  public:
+    int minJumps(int arr[], int n){
+        // Your code here
+  int farthest = 0;
+        int current = 0;
+        int jumps = 0;
+        
+        for(int i =0; i<n-1 ; i++)
+        {
+            farthest = max(farthest,arr[i]+i);
+            if(i==current)
+            {
+                current = farthest;
+                jumps++;
+            if (current >= n-1){
+            return jumps;
+
+        }
+            }
+            
+        }
+        
+       
+        return -1;
+    }
+};
