9ab1063d52de528966cb09a7fb9dc3ebc5d2e0b6
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2023-11-11 21:54:28 +0530

    commit

---
+++ isomorphic-strings.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// C++ program to check if two strings are isomorphic
+
+
+#include<bits/stdc++.h>
+
+
+using namespace std;
+#define MAX_CHARS 256
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to check if two strings are isomorphic.
+    bool areIsomorphic(string str1, string str2)
+    {
+        
+        // Your code here
+         int fre1[256]={0};
+        int fre2[256]={0};
+        if(str1.size()!=str2.size())return false;
+        for(int i=0;i<str1.size();i++){
+        if(fre1[str1[i]]!= fre2[str2[i]]){
+            return false;
+        }
+        fre1[str1[i]]=i+1;
+        fre2[str2[i]]=i+1;
+        }
+        
+        return true;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver program
+int main()
+{
+    int t;
+    cin>>t;
+    string s1,s2;
+    while (t--) {
+        cin>>s1;
+        cin>>s2;
+        Solution obj;
+        cout<<obj.areIsomorphic(s1,s2)<<endl;
+    }
+    
+    return 0;
+}
+// } Driver Code Ends
