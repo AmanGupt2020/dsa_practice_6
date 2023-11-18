afea11c0d79600632ae3eb3a73f371d33b7e1a6c
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2023-11-18 21:26:12 +0530

    commit

---
+++ Monthly-Budget.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	 int X,Y;
+	 cin>>X>>Y;
+	 if(X >= Y*30)
+	 cout<<"YES"<<endl;
+	 else
+	 cout<<"No"<<endl;
+	}
+	return 0;
+}
