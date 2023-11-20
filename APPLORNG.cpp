656f5e01c799ca488881c31c18d88442a1dcd193
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2023-11-20 22:25:25 +0530

    commit

---
+++ APPLORNG.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int c;
+	cin>>c;
+	int a,b;
+	cin>>a>>b;
+	if(a+b<=c){
+	    cout<<"YES"<<endl;
+	}
+	else{
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+}
