eef4eb8229dae186fb305aeea79820fe6b4679db
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2023-12-03 22:41:00 +0530

    commit

---
+++ FEVER.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	while(n--){
+	    int a;
+	    cin>>a;
+	    if(a>98){
+	        cout<<"YES"<<endl;
+	    }else{cout<<"NO"<<endl;}
+	}
+	return 0;
+}
