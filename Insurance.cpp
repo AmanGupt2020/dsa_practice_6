dce9d175fe9d9d01353240996f1924c2419db3de
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2023-12-05 18:46:02 +0530

    commit

---
+++ Insurance.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int t,x,y;
+	cin>>t;
+	while(t!=0)
+	{
+	    cin>>x>>y;
+	    if(x>y)
+	    cout<<y<<endl;
+	    else if(x==y)
+	    cout<<x<<endl;
+	    else
+	    cout<<x<<endl;
+	    t--;
+	}
+	return 0;
+}
