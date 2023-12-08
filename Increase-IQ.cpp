74b277971a822bda9308129c5c065177b9d88aad
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2023-12-08 20:40:40 +0530

    commit

---
+++ Increase-IQ.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    cin>>x;
+    if((x+7)>170)
+    {
+        cout<<"Yes\n";
+    }
+    else
+    {
+        cout<<"No\n";
+    }
+	// your code goes here
+	return 0;
+}
