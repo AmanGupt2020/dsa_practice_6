02382a3267cc43589c99c8a66dd0c2eb574d0042
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2023-12-12 11:55:01 +0530

    commit

---
+++ Playlist.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	int x,n;
+	std::cin >> t;
+	while(t--){
+	    std::cin >> n>>x;
+	    cout<<n/(3*x)<<endl;
+	}
+	return 0;
+}
