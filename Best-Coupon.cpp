e88b15317198c77f19347472c43159d7b8a9b481
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2023-12-12 12:09:14 +0530

    commit

---
+++ Best-Coupon.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int t;
+  cin>>t;
+  while(t--){
+      int x;
+      cin>>x;
+      if(100>(x*10)/100){
+      cout<<100<<endl;
+      }
+      else 
+  {
+      cout<<(x*10)/100<<endl;
+  }
+}
+    return 0;
+}
