f1a507bf56b8243c745f05f965dc2eb8d95f12f3
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2023-12-16 19:59:41 +0530

    commit

---
+++ Water-Park.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int W,H;
+	cin>>W>>H;
+	
+	int a=60;
+	int b=130;
+	
+	if (a<=W && b>=H){
+	    cout<<"Yes";
+	}
+	else{
+	    cout<<"No";
+	}
+    
+	
+    
+
+	return 0;
+}
