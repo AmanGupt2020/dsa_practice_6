cf902c1b61012d4cbca4e28ab87b5fbf85882bba
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2024-01-04 09:31:15 +0530

    commit

---
+++ find-element-occuring-once-when-all-other-are-present-thrice.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    int singleElement(int arr[] ,int N) {
+        // code here
+        map<long long int,int>a;
+        for(int i=0;i<N;i++)
+        {
+            a[arr[i]]++;
+        }
+        for(auto p:a)
+        {
+            if(p.second==1)
+                return p.first;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+        int arr[N];
+        
+        for(int i=0 ; i<N ; i++)
+            cin>>arr[i];
+
+        Solution ob;
+        cout<<ob.singleElement(arr,N);
+        cout<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
