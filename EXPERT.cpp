e196f9128f5d9052039b609bd7b2eddf96e68b04
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2024-01-06 20:46:26 +0530

    commit

---
+++ EXPERT.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,x;
+        cin>>n>>x;
+        if((x*100)/n>=50){
+            cout<<"yes"<<endl;
+        }
+        else{
+            cout<<"no"<<endl;
+        }
+    }
+
+    return 0;
+}
