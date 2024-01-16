ebe851e5def95a24798381894d0a535ea5395c05
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2024-01-16 22:07:42 +0530

    commit

---
+++ NIBBLE.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;cin>>n;
+	while(n--)
+	{
+	    int s;cin>>s;
+	    if(s%4==0)
+	    {
+	        cout<<"Good"<<endl;
+	    }
+	    else
+	    cout<<"Not Good"<<endl;
+	}
+	return 0;
+}
