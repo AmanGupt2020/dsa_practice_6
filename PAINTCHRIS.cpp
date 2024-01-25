19e5fcbd756749eebb9b96f182458c7825b6d98b
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2024-01-25 21:32:40 +0530

    commit

---
+++ PAINTCHRIS.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std ;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>> t ;
+	
+	while(t--) {
+	    
+	    int x , y,z ;
+	    cin>> x>> y >>z;
+	    
+	    int h = x *y;
+	    int K = z /2 ;
+	    
+	    int S =K /h;
+	    
+	    cout<< S <<endl;
+	}
+	return 0;
+
+}
