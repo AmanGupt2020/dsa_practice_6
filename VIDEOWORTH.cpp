64ad8ab908ef05723d2dbe5b4e23d9489011bfc0
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2024-01-28 19:55:33 +0530

    commit

---
+++ VIDEOWORTH.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int ans=n*24*1000;
+	    cout<<ans<<endl;
+	}
+	return 0;
+}
