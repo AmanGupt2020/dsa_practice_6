0042a0c6566cd2ebf0a9cd14a620b3d1619c409c
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2024-01-29 11:01:02 +0530

    commit

---
+++ CHANGEPOS.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++){
+	    int a,b,c,d;
+	    cin>>a>>b>>c>>d;
+	    if(a!=c && b!=d){
+	        cout<<"1"<<endl;
+	    }
+	    else{
+	        cout<<"2"<<endl;
+	    }
+	    
+	    
+	    
+	    
+	}
+return 0;
+}
