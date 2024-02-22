f099fbc765b6b904663650eef099762e4b245ce0
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2024-02-22 23:13:08 +0530

    commit

---
+++ FIZZ-BUZZ.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	
+	
+	for(int i=0;i<t;i++){
+	    int n,x,d;
+	   
+	    	cin>>n;
+         	cin>>x;
+         	cin>>d;
+         	cout<<(n/(5*x))+d;
+	 cout<<endl;
+	}
+	return 0;
+}
