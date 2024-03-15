45216189a6427cce5f84ab11ebb488fdddc60dc2
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2024-03-15 23:45:06 +0530

    Create PHONEYR.cpp

---
+++ PHONEYR.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x;
+    cin >> x;
+    int value= x % 100;
+    {
+    cout << "K" << setw(2) << setfill('0') << value << endl;
+
+    }
+}
