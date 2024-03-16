3dc9c4515b7b07ed92c61b5babc54208b3f9eef3
AMAN GUPTA <130494567+AmanGupt2020@users.noreply.github.com>
2024-03-16 23:39:26 +0530

    Create LITRATE.cpp

---
+++ LITRATE.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    float p,l;
+	    cin>>p>>l;
+	    if((l/p)>=0.75)
+	    {
+	      cout<<"yes"<<endl;
+	    }
+	    
+	    else
+	    {
+	      cout<<"no"<<endl;
+	    }
+	    
+
+	}
+	
+}
